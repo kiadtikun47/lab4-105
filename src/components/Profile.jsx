c21b8207937206a15cd63f102755ee2fcd03b53c
PpingG_47 <117506450+kiadtikun47@users.noreply.github.com>
2023-04-06 14:37:10 +0700

    Add files via upload

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import "./Profile.css";
+function Profile(){
+    return(
+        <div className="layout">
+        <img src="img/DSC_9983.jpg" alt="profile"/>
+        <h1 className="name">Ping Kiadtikul</h1>
+        <hr></hr>
+        </div>
+    )
+}
+export default Profile; 
